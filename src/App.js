7a9c3028007591f6d0fc178d1707f556fb882d14
richod1 <frimpsup@gmail.com>
2022-11-20 06:40:25 -0800

    responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import {useState} from 'react';
+
 
 function App() {
+  const [role,setRole]=useState('dev');
+
+  const [employees,setEmployees]=useState([
+    {
+      id:1,
+      name:"Degraft",
+      role:"AppDeveloper",
+      img:"https://images.pexels.com/photos/2232981/pexels-photo-2232981.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    {
+      id:2,
+      name:"Frimpong",
+      role:"IosDeveloper",
+      img:"https://images.pexels.com/photos/13743846/pexels-photo-13743846.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    },
+    {
+      id:3,
+      name:"Kweku",
+      role:"Manager",
+      img:"https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    {
+      id:4,
+      name:"Silver",
+      role:"ReactDeveloper",
+      img:"https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    {
+      id:5,
+      name:"Dollor",
+      role:"WebDeveloper",
+      img:"https://images.pexels.com/photos/2741701/pexels-photo-2741701.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    {
+      id:6,
+      name:"Daniel",
+      role:"AndriodDeveloper",
+      img:"https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    {
+      id:7,
+      name:"Monica",
+      role:"BackEndDev",
+      img:"https://images.pexels.com/photos/2167673/pexels-photo-2167673.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+    {
+      id:8,
+      name:"Morine",
+      role:"Tester QA",
+      img:"https://images.pexels.com/photos/2104252/pexels-photo-2104252.jpeg?auto=compress&cs=tinysrgb&w=400"
+    },
+  ]);
+  
+  function updateEmployee(id, newName, newRole){
+    const updatedEmployees = employees.map((employee)=>{
+      if(id == employee.id){
+        return {...employee, name: newName, role:newRole};
+      }
+
+      return employee;
+
+    });
+    setEmployees(updatedEmployees);
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+     
+      <input type="text" onChange={(e)=>{ 
+      // console.log(e.target.value);
+      setRole(e.target.value);
+       
+      } }
+      />
+      <div className="flex flex-wrap justify-center">
+       {employees.map((employees)=>{
+        console.log(employees);
+        return ( 
+        <Home 
+        key={employees.id}
+        id={employees.id}
+        name={employees.name}
+        role={employees.role}
+        img={employees.img}
+     updateEmployee={updateEmployee}
+        />);
+
+      })}
+      
+      
+      </div>
+      
+     
+      
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import EditEmployee from './EditEmployee'
+
+function Home(props) {
+  return (
+    <div>
+      <div class="min-w--[350px] mx-w[350px] m-2 py-8 px-8 max-w-sm  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+  <img class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} />
+  <div class="text-center space-y-2 sm:text-left">
+    <div class="space-y-0.5">
+      <p class="text-lg text-black font-semibold">
+        {props.name}
+      </p>
+      <p class="text-slate-500 font-medium">
+        {props.role}
+      </p>
+    </div>
+    <EditEmployee
+     name={props.name} 
+     role={props.role} 
+    updateEmployee={props.updateEmployee}/>
+    
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Home
